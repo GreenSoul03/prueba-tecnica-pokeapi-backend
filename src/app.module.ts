92a7ac2ab6dbcf24f1328efd09641e7cb9b8a1c7
David Cano <davidsantiago03.com@gmail.com>
2025-10-16 23:51:24 -0500

    correccion de conflictos al hacer merge

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { PokemonModule } from './pokemon/pokemon.module';
 import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, FavoritesModule],
-import { PokemonModule } from './pokemon/pokemon.module';
-
-@Module({
-  imports: [AuthModule, PrismaModule, PokemonModule],
+  imports: [AuthModule, PrismaModule, PokemonModule, FavoritesModule],
   controllers: [AppController],
   providers: [AppService],
 })
